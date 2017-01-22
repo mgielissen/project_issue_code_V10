abe6d9f7b0210dcae2075f177d00d4b6179c8bdb
stellaf <stella.fredo@gmail.com>
2017-01-22 23:28:24 +0100

    Add files via upload

---
+++ project_issue_code/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Michael Viriyananda
+# © 2017 Stella Fredö, adapt it to v10
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project Issue Code",
+    "version": "10.0.1.0.0",
+    'author': 'Michael Viriyananda, Stella Fredö',
+    "category": "Project Management",
+    'summary': 'Adding Field Code For Project Issue',
+    'website': 'http://github.com/mikevhe18',
+    "license": "AGPL-3",
+    "depends": [
+        "project_issue",
+    ],
+    "data": [
+        "data/project_issue_sequence.xml",
+        "views/project_issue_view.xml",
+    ],
+    "installable": True,
+}
---
+++ project_issue_code/hooks.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# © 2016 Michael Viriyananda
+# 2017 upgraded it to odoo 10, and add some sql safty guard command, by stella.fredo@gmail.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import SUPERUSER_ID
+from odoo import api
+from odoo import pre_init_hook, post_init_hook
+
+def pre_init_hook(cr):
+    create_code_equal_to_id(cr)
+
+def post_init_hook(cr,registry):
+    assign_old_sequences(cr, registry)  
+
+def create_code_equal_to_id(cr):
+        """
+        With this pre-init-hook we want to avoid error when creating the UNIQUE
+        code constraint when the module is installed and before the post-init-hook
+        is launched.
+        """
+        cr.execute('ALTER TABLE project_issue drop column if exists issue_code; '
+                   'ALTER TABLE project_issue ADD COLUMN issue_code character varying;')
+        cr.execute('UPDATE project_issue SET issue_code = id;')
+
+def assign_old_sequences(cr, registry):
+        """
+        This post-init-hook will update all existing issue assigning them the
+        corresponding sequence code.
+        """
+        issue_obj = registry['project.issue']
+        issue_ids = issue_obj.search(cr, SUPERUSER_ID, [], order="id")
+        for issue_id in issue_ids:
+            cr.execute('UPDATE project_issue '
+                   'SET issue_code = \'%s\' '
+                   'WHERE id = %d;' %
+                   (self.env['ir.sequence'].next_by_code('project.issue'),
+                    issue_id))

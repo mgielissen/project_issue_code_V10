5cb2178b7de07b4f5758529b78ec06b88ddf9952
stellaf <stella.fredo@gmail.com>
2017-04-15 22:59:12 +0200

    Update __openerp__.py

--- project_issue_code/__openerp__.py
+++ project_issue_code/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Project Issue Code",
-    "version": "10.0.1.0.0",
+    "version": "10.0.2.0.0",
     'author': 'Michael Viriyananda, Stella Fredö',
     "category": "Project Management",
     'summary': 'Adding Field Code For Project Issue',
